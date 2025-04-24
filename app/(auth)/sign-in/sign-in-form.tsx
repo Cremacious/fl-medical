afe727a2f7e7a03e4b6a977a758704d5f112767a
Cremacious <chrismackall3@gmail.com>
2025-04-24 16:00:13 -0400

    Created form and page for sign up and sign in

---
+++ app/(auth)/sign-in/sign-in-form.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Input } from '@/components/ui/input';
+import Link from 'next/link';
+
+const SignInForm = () => {
+  return (
+    <section className="py-32">
+      <div className="container">
+        <div className="flex flex-col gap-4">
+          <div className="mx-auto w-full max-w-sm rounded-md p-6 shadow">
+            <div className="mb-6 flex flex-col items-center">
+              {/* <a href={logo.url} className="mb-6 flex items-center gap-2">
+                <img src={logo.src} className="max-h-8" alt={logo.alt} />
+              </a> */}
+              <h1 className="mb-2 text-2xl font-bold">Sign In</h1>
+              <p className="text-muted-foreground">SUBHEADING</p>
+            </div>
+            <div>
+              <div className="grid gap-4">
+                <Input type="email" placeholder="Enter your email" required />
+                <div>
+                  <Input
+                    type="password"
+                    placeholder="Enter your password"
+                    required
+                  />
+                </div>
+                <div className="flex justify-between">
+                  <div className="flex items-center space-x-2">
+                    <Checkbox
+                      id="remember"
+                      className="border-muted-foreground"
+                    />
+                    <label
+                      htmlFor="remember"
+                      className="text-sm leading-none font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+                  <a href="#" className="text-sm text-primary hover:underline">
+                    Forgot password
+                  </a>
+                </div>
+                <Button type="submit" className="mt-2 w-full">
+                  LOGIN TEXT
+                </Button>
+                {/* <Button variant="outline" className="w-full">
+                  <FcGoogle className="mr-2 size-5" />
+                  {googleText}
+                </Button> */}
+              </div>
+              <div className="mx-auto mt-8 flex justify-center gap-1 text-sm text-muted-foreground">
+                <p>Already have an account?</p>
+                <Link href="/sign-up" className="font-medium text-primary">
+                  Sign Up
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SignInForm;
