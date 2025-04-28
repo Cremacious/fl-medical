2f7aee19840276e2a683527042c7ae570b7ff307
Cremacious <chrismackall3@gmail.com>
2025-04-28 16:08:32 -0400

    Edited details on total section

--- app/(root)/dashboard/history/[id]/page.tsx
+++ app/(root)/dashboard/history/[id]/page.tsx
@@ -1,14 +1,37 @@
 import PurchaseCard from './purchase-card';
+import { Card, CardHeader, CardContent, CardTitle } from '@/components/ui/card';
 
 const ViewPurchase = async (props: { params: Promise<{ id: string }> }) => {
   const { id } = await props.params;
   return (
     <>
       <div className="newPage">
-        Dispensary name - total price - date
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="bg-blue-100 p-4 rounded-lg shadow-lg">
-            <div className="flex flex-col gap-4">Prices</div>
+          <div className="">
+            <div className="">
+              <Card>
+                <CardHeader>
+                  <CardTitle>04/20/2025</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="flex flex-col gap-2">
+                    <div className="flex flex-row justify-between border-b-2 border-b-slate-200 pb-2">
+                      <div className="font-bold">Dispensary</div>
+                      <div>Trulieve</div>
+                    </div>
+                    <div className="flex flex-row justify-between border-b-2  border-b-slate-200 pb-2">
+                      <div className="font-bold">Items</div>
+                      <div>4</div>
+                    </div>
+
+                    <div className="flex flex-row justify-between border-b-2  border-b-slate-200 pb-2">
+                      <div className="font-bold">Total</div>
+                      <div>$120</div>
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
           </div>
           <div className="bg-blue-100 p-4 rounded-lg shadow-lg">
             <div className="flex flex-col gap-4">
