bbb18937c99ef695dee8cebda3362cc76776f858
Cremacious <chrismackall3@gmail.com>
2025-04-28 18:55:27 -0400

    Fixed alignment on larger screens

--- app/(root)/dashboard/history/history-table.tsx
+++ app/(root)/dashboard/history/history-table.tsx
@@ -29,11 +29,13 @@ const HistoryTable = () => {
         <TableBody>
           <Link legacyBehavior href="/dashboard/history/1">
             <TableRow className="cursor-pointer hover:bg-slate-100">
-              <TableCell className="">04/20/2025</TableCell>
-              <TableCell className="">Trulieve</TableCell>
-              <TableCell className="flex-1 break-words">$2500.00</TableCell>
-              <TableCell className="">3</TableCell>
-              <TableCell className="">
+              <TableCell className="text-center">04/20/2025</TableCell>
+              <TableCell className="text-center">Trulieve</TableCell>
+              <TableCell className="flex-1 text-center break-words">
+                $2500.00
+              </TableCell>
+              <TableCell className="text-center">3</TableCell>
+              <TableCell className="text-center">
                 <Button>Delete</Button>
               </TableCell>
             </TableRow>
