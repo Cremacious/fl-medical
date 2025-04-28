1e53e9fc157a0b070e15be88bc08a67a5dfad4df
Cremacious <chrismackall3@gmail.com>
2025-04-28 18:54:13 -0400

    Row on table now links to purchase page

--- app/(root)/dashboard/history/history-table.tsx
+++ app/(root)/dashboard/history/history-table.tsx
@@ -19,29 +19,25 @@ const HistoryTable = () => {
         <TableCaption>A list of your recent purchases.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="text-center"></TableHead>
-            <TableHead className="text-center w-[100px]">Date</TableHead>
+            <TableHead className="text-center">Date</TableHead>
             <TableHead className="text-center">Dispensary</TableHead>
             <TableHead className="text-center">Price</TableHead>
-            <TableHead className="text-center">Item Qty</TableHead>
+            <TableHead className="text-center">Qty</TableHead>
             <TableHead className="text-center">Delete</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="text-center">
-              <Button asChild>
-                <Link href="/dashboard/history/1">View</Link>
-              </Button>
-            </TableCell>
-            <TableCell className="font-medium">04/20/2025</TableCell>
-            <TableCell className="text-center">Trulieve</TableCell>
-            <TableCell className="text-center">$250.00</TableCell>
-            <TableCell className="text-center">3</TableCell>
-            <TableCell className="text-center">
-              <Button>Delete</Button>
-            </TableCell>
-          </TableRow>
+          <Link legacyBehavior href="/dashboard/history/1">
+            <TableRow className="cursor-pointer hover:bg-slate-100">
+              <TableCell className="">04/20/2025</TableCell>
+              <TableCell className="">Trulieve</TableCell>
+              <TableCell className="flex-1 break-words">$2500.00</TableCell>
+              <TableCell className="">3</TableCell>
+              <TableCell className="">
+                <Button>Delete</Button>
+              </TableCell>
+            </TableRow>
+          </Link>
         </TableBody>
       </Table>
     </>
