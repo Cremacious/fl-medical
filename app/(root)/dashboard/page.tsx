5fc2102399b7716103200565b347496af2a78879
Cremacious <chrismackall3@gmail.com>
2025-04-29 12:39:12 -0400

    Began work on pie chart

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -1,16 +1,37 @@
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import UsageChart from '@/components/usage-chart';
 
 const DashboardPage = () => {
   return (
     <>
-      Dashboard page
-      <Button asChild>
-        <Link href="/dashboard/add">Add</Link>
-      </Button>
-      <Button asChild>
-        <Link href="/dashboard/profile">Profile</Link>
-      </Button>
+      <div className="newPage">
+        <div className="text-slate-800 body-font">
+          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+              <img
+                className="object-cover object-center rounded"
+                alt="hero"
+                src="https://dummyimage.com/720x600"
+              />
+            </div>
+            <div className="flex flex-col justify-center">
+              <h1 className="text-center">Welcome, name!</h1>
+              <UsageChart />
+              {/* <p className="mb-8 text-center">
+                Copper mug try-hard pitchfork pour-over freegan heirloom neutra
+                air plant cold-pressed tacos poke beard tote bag. Heirloom echo
+                park mlkshk tote bag selvage hot chicken authentic tumeric
+                truffaut hexagon try-hard chambray.
+              </p> */}
+              <div className="flex flex-row justify-center gap-4">
+                <Button>Add Purchase</Button>
+                <Button>Add Purchase</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
