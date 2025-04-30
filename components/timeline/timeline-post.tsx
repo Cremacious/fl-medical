49887d88596493dd793e04df61bdfd3d993611f0
Cremacious <chrismackall3@gmail.com>
2025-04-29 20:53:52 -0400

    Restyled post card

--- components/timeline/timeline-post.tsx
+++ components/timeline/timeline-post.tsx
@@ -9,19 +9,32 @@ const TimelinePost = () => {
   return (
     <Link href="/post/3">
       <div className="flex justify-center items-center">
-        <div className="max-w-xs container bg-white rounded-xl shadow-lg animationEffect">
+        <div className="max-w-xs container bg-[#116a7e] text-white rounded-xl shadow-lg animationEffect">
           <div>
-            <span className="text-slate-800 text-xs font-bold inline-block mt-4 ml-2 py-1.5 px-2 cursor-pointer">
+            <span className="text-white text-xs font-bold inline-block mt-4 ml-2 py-1.5 px-2 cursor-pointer bg-[#eb7808] rounded">
               04/10/2023
             </span>
-            <h1 className="text-lg my-2 ml-4 font-bold text-gray-800 cursor-pointer hover:text-gray-900 transition duration-100">
+            <h1 className="text-lg my-2 ml-4 font-bold text-white cursor-pointer hover:text-gray-200 transition duration-100">
               Tonight after work
             </h1>
           </div>
           <Image className="w-full cursor-pointer" src={stock} alt="image" />
           <div className="flex p-4 justify-evenly items-center flex-wrap">
-            <Badge className="m-2">Strain Name</Badge>
-            <Badge className="m-2">Strain Name</Badge>
+            <Badge className="m-2 bg-[#f3a714] text-slate-800">
+              <h3 className="font-bold text-[13px]">Northern Lights</h3>
+            </Badge>
+            <Badge className="m-2 bg-[#f3a714] text-slate-800">
+              <h3 className="font-bold text-[13px]">Northern Lights</h3>
+            </Badge>
+            <Badge className="m-2 bg-[#f3a714] text-slate-800">
+              <h3 className="font-bold text-[13px]">Northern Lights</h3>
+            </Badge>
+            <Badge className="m-2 bg-[#f3a714] text-slate-800">
+              <h3 className="font-bold text-[13px]">Northern Lights</h3>
+            </Badge>
+            <Badge className="m-2 bg-[#f3a714] text-slate-800">
+              <h3 className="font-bold text-[13px]">Northern Lights</h3>
+            </Badge>
           </div>
         </div>
       </div>
