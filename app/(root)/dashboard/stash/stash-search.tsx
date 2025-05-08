39ea3cd2b73004042b8cded18432751c0f534140
Cremacious <chrismackall3@gmail.com>
2025-05-08 11:59:58 -0400

    Styling

--- app/(root)/dashboard/stash/stash-search.tsx
+++ app/(root)/dashboard/stash/stash-search.tsx
@@ -11,6 +11,8 @@ import {
 import { useState } from 'react';
 import { StashItem } from '@/lib/types';
 import ProductCard from '@/components/shared/products/product-card';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 type StashSearchProps = {
   stashItems: StashItem[];
@@ -52,51 +54,58 @@ const StashSearch: React.FC<StashSearchProps> = ({ stashItems }) => {
 
   return (
     <>
-      <div className="flex flex-col md:flex-row gap-4 mb-4 justify-between">
-        {/* Search by Name */}
-        <Input
-          type="text"
-          name="name"
-          placeholder="Search by name"
-          value={searchParams.name}
-          onChange={handleInputChange}
-          className="bg-white w-full md:w-[250px]"
-        />
+      <div className="flex flex-col md:flex-row gap-2 mb-4 justify-between">
+        <div className="flex justify-center w-full lg:w-[200px] flex-row gap-2 md:gap-4 ">
+          <Button asChild className="w-[300px]">
+            <Link href="/dashboard/stash/add">Add New</Link>
+          </Button>
+        </div>
 
-        <div className="flex flex-row gap-4 justify-center">
-          <Select
-            value={searchParams.category}
-            onValueChange={(value) =>
-              setSearchParams((prev) => ({ ...prev, category: value }))
-            }
-          >
-            <SelectTrigger className="bg-white w-[150px]">
-              <SelectValue placeholder="Category" />
-            </SelectTrigger>
+        <div className="flex flex-col lg:flex-row  gap-2 lg:gap-4">
+          <div className="flex flex-row gap-4 justify-center ">
+            <Select
+              value={searchParams.category}
+              onValueChange={(value) =>
+                setSearchParams((prev) => ({ ...prev, category: value }))
+              }
+            >
+              <SelectTrigger className="bg-white w-[140px]">
+                <SelectValue placeholder="Category" />
+              </SelectTrigger>
 
-            <SelectContent>
-              <SelectItem value="all">All Categories</SelectItem>
-              <SelectItem value="flower">Flower</SelectItem>
-              <SelectItem value="vape">Vape</SelectItem>
-              <SelectItem value="edible">Edible</SelectItem>
-            </SelectContent>
-          </Select>
-          <Select
-            value={searchParams.type}
-            onValueChange={(value) =>
-              setSearchParams((prev) => ({ ...prev, type: value }))
-            }
-          >
-            <SelectTrigger className="bg-white w-[120px]">
-              <SelectValue placeholder="Type" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="all">All Types</SelectItem>
-              <SelectItem value="sativa">Sativa</SelectItem>
-              <SelectItem value="indica">Indica</SelectItem>
-              <SelectItem value="hybrid">Hybrid</SelectItem>
-            </SelectContent>
-          </Select>
+              <SelectContent>
+                <SelectItem value="all">All Categories</SelectItem>
+                <SelectItem value="flower">Flower</SelectItem>
+                <SelectItem value="vape">Vape</SelectItem>
+                <SelectItem value="edible">Edible</SelectItem>
+              </SelectContent>
+            </Select>
+            <Select
+              value={searchParams.type}
+              onValueChange={(value) =>
+                setSearchParams((prev) => ({ ...prev, type: value }))
+              }
+            >
+              <SelectTrigger className="bg-white w-[120px]">
+                <SelectValue placeholder="Type" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">All Types</SelectItem>
+                <SelectItem value="sativa">Sativa</SelectItem>
+                <SelectItem value="indica">Indica</SelectItem>
+                <SelectItem value="hybrid">Hybrid</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <Input
+            type="text"
+            name="name"
+            placeholder="Search by name"
+            value={searchParams.name}
+            onChange={handleInputChange}
+            className="bg-white  md:w-[250px]"
+          />
         </div>
       </div>
 
