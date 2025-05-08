693fc986ed9ef8539793be8bc959a2fb799d61bc
Cremacious <chrismackall3@gmail.com>
2025-05-08 14:30:25 -0400

    Styling

--- app/(root)/dashboard/stash/edit/[id]/page.tsx
+++ app/(root)/dashboard/stash/edit/[id]/page.tsx
@@ -5,20 +5,24 @@ const EditStashPage = async (props: { params: Promise<{ id: string }> }) => {
   const { id } = await props.params;
   const item = await getStashItemById(id);
   return (
-    <>
-      <EditStashForm
-        item={{
-          ...item,
-          category: item.category ?? undefined,
-          type: item.type ?? undefined,
-          size: item.size ?? undefined,
-          thc: item.thc ?? undefined,
-          cbd: item.cbd ?? undefined,
-          lineage: item.lineage ?? undefined,
-          thoughts: item.thoughts ?? undefined,
-        }}
-      />
-    </>
+    <div className="newPage">
+      <div className="customBlue roundShadow py-8 px-4">
+        <div className="customCyan roundShadow p-4">
+          <EditStashForm
+            item={{
+              ...item,
+              category: item.category ?? undefined,
+              type: item.type ?? undefined,
+              size: item.size ?? undefined,
+              thc: item.thc ?? undefined,
+              cbd: item.cbd ?? undefined,
+              lineage: item.lineage ?? undefined,
+              thoughts: item.thoughts ?? undefined,
+            }}
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
