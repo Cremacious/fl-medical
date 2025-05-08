2914098293a38b8b855489ffcf38f9ec13971276
Cremacious <chrismackall3@gmail.com>
2025-05-08 12:35:20 -0400

    Pagination working

--- app/(root)/dashboard/stash/stash-search.tsx
+++ app/(root)/dashboard/stash/stash-search.tsx
@@ -18,6 +18,8 @@ type StashSearchProps = {
   stashItems: StashItem[];
 };
 
+const ITEMS_PER_PAGE = 12;
+
 const StashSearch: React.FC<StashSearchProps> = ({ stashItems }) => {
   const [searchParams, setSearchParams] = useState({
     name: '',
@@ -25,11 +27,14 @@ const StashSearch: React.FC<StashSearchProps> = ({ stashItems }) => {
     type: 'all',
   });
 
+  const [currentPage, setCurrentPage] = useState(1);
+
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
     const { name, value } = e.target;
     setSearchParams((prev) => ({ ...prev, [name]: value }));
+    setCurrentPage(1);
   };
 
   const filteredItems = stashItems.filter((item) => {
@@ -50,6 +55,16 @@ const StashSearch: React.FC<StashSearchProps> = ({ stashItems }) => {
     }
     return true;
   });
+
+  const totalPages = Math.ceil(filteredItems.length / ITEMS_PER_PAGE);
+  const paginatedItems = filteredItems.slice(
+    (currentPage - 1) * ITEMS_PER_PAGE,
+    currentPage * ITEMS_PER_PAGE
+  );
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
 
   return (
     <>
@@ -109,11 +124,9 @@ const StashSearch: React.FC<StashSearchProps> = ({ stashItems }) => {
           />
         </div>
       </div>
-
-      {/* Display Filtered Items */}
       <div className=" customCyan p-6 roundShadow min-h-screen">
         <div className="grid gap-4 grid-cols-1 md:grid-cols-4 justify-center">
-          {filteredItems.map((item) => (
+          {paginatedItems.map((item) => (
             <ProductCard
               item={{
                 id: item.id || '',
@@ -131,6 +144,24 @@ const StashSearch: React.FC<StashSearchProps> = ({ stashItems }) => {
               key={item.id || ''}
             />
           ))}
+        </div>
+        <div className="flex justify-center mt-8">
+          <div className="flex items-center mr-2">Page: </div>
+          <div className="flex gap-2">
+            {Array.from({ length: totalPages }, (_, index) => (
+              <button
+                key={index + 1}
+                onClick={() => handlePageChange(index + 1)}
+                className={`px-4 py-2 rounded ${
+                  currentPage === index + 1
+                    ? 'bg-[#f7c062] text-slate-800'
+                    : 'bg-[#116a7e] text-white'
+                }`}
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </>
